84510651eb0da1df64494591023435b2afff6eca
Not-Sarthak <notsarthakshah@gmail.com>
2025-06-07 18:30:08 +0530

    chore: ONE LAST TIME

--- components/buttons/button.tsx
+++ components/buttons/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2",
         sm: "h-8 px-3 py-1.5 text-sm",
         lg: "h-10 px-6 py-2.5 text-base",
-        icon: "p-2",
+        icon: "p-2", 
       }, 
     },
     defaultVariants: { 
