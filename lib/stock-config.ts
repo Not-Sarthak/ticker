0af44399591970d9a321bf12411d4f53ab3961ab
Not-Sarthak <notsarthakshah@gmail.com>
2025-06-08 14:26:07 +0530

    fix: stock order

--- lib/stock-config.ts
+++ lib/stock-config.ts
@@ -1,6 +1,26 @@
 export const stockTokens = [
   {
     id: 1,
+    chainId: 8453,
+    tokenAddress: "0xDEc933e2392AD908263e70A386fbF34e703Ffe8F",
+    name: "Wrapped bCOIN",
+    symbol: "WBCOIN",
+    decimals: 18,
+    logoURI:
+      "https://assets.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
+  },
+  {
+    id: 2,
+    chainId: 100,
+    tokenAddress: "0x1f82284c1658ad71c576f7230e6c2dee7901c1fa",
+    name: "Wrapped bTSLA",
+    symbol: "WBTSLA",
+    decimals: 18,
+    logoURI:
+      "https://coin-images.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
+  },
+  {
+    id: 3,
     chainId: 137,
     tokenAddress: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
     name: "APPLE INC.",
@@ -9,7 +29,7 @@ export const stockTokens = [
     logoURI: "https://d153cgr8o6wwrb.cloudfront.net/aapl.svg"
   },
   {
-    id: 2,
+    id: 4,
     chainId: 137,
     tokenAddress: "0xfbad91131d74911c431ba6e35164f23f6295de74",
     name: "Blackrock Inc",
@@ -18,7 +38,7 @@ export const stockTokens = [
     logoURI: "https://d153cgr8o6wwrb.cloudfront.net/BlackRock.svg"
   },
   {
-    id: 3,
+    id: 5,
     chainId: 137,
     tokenAddress: "0x916cb41692729a8d620dab7b0a7868f673b257c3",
     name: "Coinbase Global Inc - Class A",
@@ -27,7 +47,7 @@ export const stockTokens = [
     logoURI: "https://d153cgr8o6wwrb.cloudfront.net/coin.svg"
   },
   {
-    id: 4,
+    id: 6,
     chainId: 137,
     tokenAddress: "0x45dda5490a585005991ac03ab1c31304ad171502",
     name: "Coupang Inc - Class A",
@@ -36,16 +56,7 @@ export const stockTokens = [
     logoURI: "https://d153cgr8o6wwrb.cloudfront.net/Coupang.svg"
   },
   {
-    id: 5,
-    chainId: 137,
-    tokenAddress: "0x4cc746a365ae257d49d36a03eb1ef865054cb16b",
-    name: "Intel Corp",
-    symbol: "INTC",
-    decimals: 18,
-    logoURI: "https://d153cgr8o6wwrb.cloudfront.net/Intel.svg"
-  },
-  {
-    id: 6,
+    id: 7,
     chainId: 137,
     tokenAddress: "0x76e31f3d9d3f1f7af7c2b0db8aef25cedc87215a",
     name: "Microsoft Corporation",
@@ -54,7 +65,7 @@ export const stockTokens = [
     logoURI: "https://d153cgr8o6wwrb.cloudfront.net/Microsoft.svg"
   },
   {
-    id: 7,
+    id: 8,
     chainId: 137,
     tokenAddress: "0xea942a7db5b0fa5beaac60edf14cac63e488ea48",
     name: "Microstrategy Inc - Class A",
@@ -63,7 +74,7 @@ export const stockTokens = [
     logoURI: "https://d153cgr8o6wwrb.cloudfront.net/MicroStrategy.svg"
   },
   {
-    id: 8,
+    id: 9,
     chainId: 137,
     tokenAddress: "0x267fc8b95345916c9740cbc007ed65c71b052395",
     name: "NVIDIA Corp",
@@ -72,7 +83,7 @@ export const stockTokens = [
     logoURI: "https://d153cgr8o6wwrb.cloudfront.net/NVIDIA.svg"
   },
   {
-    id: 9,
+    id: 10,
     chainId: 137,
     tokenAddress: "0xb635cd9ff04eaa9d1cb394acc2c93ae60cd64dd2",
     name: "TESLA INC. DL - 001",
@@ -81,7 +92,7 @@ export const stockTokens = [
     logoURI: "https://assets.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458"
   },
   {
-    id: 10,
+    id: 11,
     chainId: 137,
     tokenAddress: "0xdcfdca64194945a44f092f8ed000245146afcdd6",
     name: "xGold",
@@ -90,7 +101,7 @@ export const stockTokens = [
     logoURI: "https://d153cgr8o6wwrb.cloudfront.net/GoldBar.svg"
   },
   {
-    id: 11,
+    id: 12,
     chainId: 1,
     tokenAddress: "0x1f82284c1658ad71c576f7230e6c2dee7901c1fa",
     name: "Wrapped bTSLA",
@@ -98,16 +109,6 @@ export const stockTokens = [
     decimals: 18,
     logoURI:
       "https://assets.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
-  },
-  {
-    id: 12,
-    chainId: 100,
-    tokenAddress: "0x1f82284c1658ad71c576f7230e6c2dee7901c1fa",
-    name: "Wrapped bTSLA",
-    symbol: "WBTSLA",
-    decimals: 18,
-    logoURI:
-      "https://coin-images.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
   },
   {
     id: 13,
@@ -131,16 +132,6 @@ export const stockTokens = [
   },
   {
     id: 15,
-    chainId: 8453,
-    tokenAddress: "0xDEc933e2392AD908263e70A386fbF34e703Ffe8F",
-    name: "Wrapped bCOIN",
-    symbol: "WBCOIN",
-    decimals: 18,
-    logoURI:
-      "https://assets.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
-  },
-  {
-    id: 16,
     chainId: 100,
     tokenAddress: "0xDEc933e2392AD908263e70A386fbF34e703Ffe8F",
     name: "Wrapped bCOIN",
@@ -150,7 +141,7 @@ export const stockTokens = [
       "https://assets.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
   },
   {
-    id: 17,
+    id: 16,
     chainId: 1,
     tokenAddress: "0xDEc933e2392AD908263e70A386fbF34e703Ffe8F",
     name: "Wrapped bCOIN",
@@ -160,8 +151,18 @@ export const stockTokens = [
       "https://assets.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
   },
   {
-    id: 18,
+    id: 17,
     chainId: 137,
+    tokenAddress: "0xDEc933e2392AD908263e70A386fbF34e703Ffe8F",
+    name: "Wrapped bCOIN",
+    symbol: "WBCOIN",
+    decimals: 18,
+    logoURI:
+      "https://assets.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
+  },
+  {
+    id: 18,
+    chainId: 43114,
     tokenAddress: "0xDEc933e2392AD908263e70A386fbF34e703Ffe8F",
     name: "Wrapped bCOIN",
     symbol: "WBCOIN",
@@ -172,21 +173,20 @@ export const stockTokens = [
   {
     id: 19,
     chainId: 43114,
-    tokenAddress: "0xDEc933e2392AD908263e70A386fbF34e703Ffe8F",
-    name: "Wrapped bCOIN",
-    symbol: "WBCOIN",
-    decimals: 18,
-    logoURI:
-      "https://assets.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
-  },
-  {
-    id: 20,
-    chainId: 43114,
     tokenAddress: "0x1f82284c1658ad71c576f7230e6c2dee7901c1fa",
     name: "Wrapped bTSLA",
     symbol: "WBTSLA",
     decimals: 18,
     logoURI:
       "https://assets.coingecko.com/coins/images/54428/large/bTSLA_200p.png?1740041458",
+  },
+  {
+    id: 20,
+    chainId: 137,
+    tokenAddress: "0x4cc746a365ae257d49d36a03eb1ef865054cb16b",
+    name: "Intel Corp",
+    symbol: "INTC",
+    decimals: 18,
+    logoURI: "https://d153cgr8o6wwrb.cloudfront.net/Intel.svg"
   },
 ];
