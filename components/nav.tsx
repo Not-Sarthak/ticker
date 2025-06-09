3b86d64dddd7dce9ba3ede6e178867f913c4e090
Not-Sarthak <notsarthakshah@gmail.com>
2025-06-09 10:24:15 +0530

    chore: remove maintainacne modal

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,21 +3,11 @@ import WalletConnector from "./buttons/wallet-connector";
 import Image from "next/image";
 
 const Nav = () => {
-  const [showTrafficAlert, setShowTrafficAlert] = useState(true);
 
   return (
     <>
-      {showTrafficAlert && (
-        <div className="fixed top-0 left-0 right-0 z-50 bg-[#1e2024] border-b border-[#2e2f34] md:p-3 p-1">
-          <div className="max-w-3xl mx-auto flex items-center justify-center text-center">
-            <p className="text-[#ffd698] text-sm">
-              High API Volume - We're working with providers to increase capacity. Thank you for your patience!
-            </p>
-          </div>
-        </div>
-      )}
       <nav
-        className={`fixed w-full z-40 ${showTrafficAlert ? 'top-12' : 'top-0'} px-6 py-4 flex justify-between items-center backdrop-blur-3xl`}
+        className={`fixed w-full z-40 top-0 px-6 py-4 flex justify-between items-center backdrop-blur-3xl`}
       >
         <a href="/" className="w-fit text-white flex items-center gap-2 group">
           <Image src="/splash.png" alt="Ticker" width={40} height={40} />
