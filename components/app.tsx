028c0473c465b5d1f47c55d9a0cc44d7a2ff7c71
Not-Sarthak <notsarthakshah@gmail.com>
2025-06-09 11:01:00 +0530

    fix: add sdk effect back

--- components/app.tsx
+++ components/app.tsx
@@ -1,10 +1,27 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import sdk from "@farcaster/frame-sdk";
+
 import SwapUI from "./swap-interface/swap";
 import Preview from "./background/background";
 import Nav from "./nav";
 
 export default function App() {
+
+  const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+  const [farcasterUserContext, setFarcasterUserContext] = useState<any>();
+
+  useEffect(() => {
+    const load = async () => {
+      setFarcasterUserContext(await sdk.context);
+      sdk.actions.ready();
+    };
+    if (sdk && !isSDKLoaded) {
+      setIsSDKLoaded(true);
+      load();
+    }
+  }, [isSDKLoaded]);
 
   return (
     <div className="min-h-screen bg-[var(--background)] relative">
